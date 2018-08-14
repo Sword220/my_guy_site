7b9f618ca9a797970b3dee8c9515ac0537042143
Pete Sword <pete.sword@yahoo.com>
2018-08-14 11:21:13 -0600

    change domain for contact mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: 'myguydrainsolutions@gmail.com',  
   port: 587,
-  domain: 'myguydrainsolutions.com',
+  domain: 'my-guy-drain-solutions.herokuapp.com',
   user_name: ENV['MAIL_LOGIN'],
   password: ENV['MAIL_PASSWORD'],
   authentication: 'plain',
