23377f127a774158312d232810a37fe8ce4904cf
Pete Sword <pete.sword@yahoo.com>
2018-08-15 12:31:53 -0600

    myguy domain for mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,19 +53,17 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "my-guy2_#{Rails.env}"
-  
-#####################
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address: 'myguydrainsolutions@gmail.com',  
   port: 587,
-  domain: 'www.myguydrainsolutions.com.herokudns.com',
+  domain: 'www.myguydrainsolutions.com',
   user_name: ENV['MAIL_LOGIN'],
   password: ENV['MAIL_PASSWORD'],
   authentication: 'plain',
   enable_starttls_auto: true
   }
-#####################
 
   config.action_mailer.perform_caching = false
 
